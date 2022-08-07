b7b7651edf8fa3a24d1ffe20a0dd19e20c626653
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-08-07 17:29:38 +0300

    validate New Usernames And Emails

--- src/main/java/com/supportportal/service/Impl/UserServiceImpl.java
+++ src/main/java/com/supportportal/service/Impl/UserServiceImpl.java
@@ -2,9 +2,13 @@ package com.supportportal.service.Impl;
 
 import com.supportportal.domain.User;
 import com.supportportal.domain.UserPrincipal;
+import com.supportportal.exception.domain.EmailExistsException;
+import com.supportportal.exception.domain.UserNameExistException;
+import com.supportportal.exception.domain.UserNotFoundException;
 import com.supportportal.repo.UserRepo;
 import com.supportportal.service.UserService;
 //import org.slf4j.Logger;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -45,8 +49,46 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
 
     @Override
-    public User register(String firstName, String username, String email) {
+    public User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, UserNameExistException, EmailExistsException {
+        validateNewUsernameAndEmail(StringUtils.EMPTY,username,email);
         return null;
+    }
+
+    /**
+     * check UserNotFoundException, UserNameExistException, EmailExistsException
+     * @param currentUsername
+     * @param newUsername
+     * @param newEmail
+     * @return
+     * @throws UserNotFoundException
+     * @throws UserNameExistException
+     * @throws EmailExistsException
+     */
+    private User validateNewUsernameAndEmail(String currentUsername,String newUsername,String newEmail) throws UserNotFoundException, UserNameExistException, EmailExistsException {
+        if (StringUtils.isNotBlank(currentUsername)){
+            User currentUser=findUserByUsername(currentUsername);
+            if (currentUser==null){
+                throw new UserNotFoundException("No user found by username"+ currentUsername);
+            }
+            User userByUsername =findUserByUsername(newUsername);//check if username already been used in my DB
+            if(userByUsername!=null && currentUser.getId().equals(userByUsername.getId())){
+                throw new UserNameExistException("Username already exists");
+            }
+            User userByEmail =findUserByEmail(newEmail);//check if username already been used in my DB
+            if(userByEmail!=null && currentUser.getId().equals(userByEmail.getId())){
+                throw new EmailExistsException("Username already exists");
+            }
+            return currentUser;
+        }else {
+            User userByUsername =findUserByUsername(newUsername);
+            if (userByUsername!=null){ throw new UserNameExistException("Username already exists"); }
+
+            User userByEmail =findUserByEmail(newEmail);
+            if(userByEmail!=null){
+                throw new EmailExistsException("Username already exists");
+            }
+            return null;
+        }
     }
 
     @Override
@@ -55,7 +97,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     }
 
     @Override
-    public User findByUsername(String username) {
+    public User findUserByUsername(String username) {
         return null;
     }
 
--- src/main/java/com/supportportal/service/UserService.java
+++ src/main/java/com/supportportal/service/UserService.java
@@ -1,14 +1,17 @@
 package com.supportportal.service;
 
 import com.supportportal.domain.User;
+import com.supportportal.exception.domain.EmailExistsException;
+import com.supportportal.exception.domain.UserNameExistException;
+import com.supportportal.exception.domain.UserNotFoundException;
 
 import java.util.List;
 
 public interface UserService {
 
-    User register(String firstName,String username,String email);
+    User register(String firstName, String lastName,String username,String email) throws UserNotFoundException, UserNameExistException, EmailExistsException;
     List<User>getUsers();
-    User findByUsername(String username);
+    User findUserByUsername(String username);
     User findUserByEmail(String email);
 
 
