231d3809de3b3ee5a9c943063be24a1bf86ee5db
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-09-16 15:40:24 +0300

    prevent brute force attack service using google dependency for  cache

--- src/main/java/com/supportportal/service/LoginAttemptService.java
+++ src/main/java/com/supportportal/service/LoginAttemptService.java
@@ -5,20 +5,27 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutionException;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 
+
+//prevent brute force attack service using google dependency
+//(59 8:00)
 @Service
 public class LoginAttemptService {
     public static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
-    public static final int ATTEMPT_INCREMENT = 5;
+    public static final int ATTEMPT_INCREMENT = 1;
     private LoadingCache<String,Integer> loginAttemptCache; //LoadingCache come from google.guava dependency
     //LoadingCache key- the username,value-the num of attempts
 
 
     //define con to initialize the cash
 
+    /**
+     * expireAfterWrite- the time cache will expire
+     * maximumSize the num of entries the cache can support
+     */
     public LoginAttemptService(){
         super();
         loginAttemptCache= CacheBuilder.newBuilder().expireAfterWrite(15, MINUTES)
@@ -29,5 +36,36 @@ public class LoginAttemptService {
                 });
     }
 
+    //move user-key and value from cache
+    public void evictUserFromLoginAttemptCache(String username){
+        loginAttemptCache.invalidate(username);
+    }
+
+
+    public void addUserToLoginAttemptCache(String username) throws ExecutionException{
+        int attempts=0;
+            attempts=loginAttemptCache.get(username) + ATTEMPT_INCREMENT  ;
+            loginAttemptCache.put(username,attempts);
+    }
+
+    /**
+     *
+     * @param username is the key
+     * @return boolean answer if the key(username) has value bigger or equal to MAXIMUM_NUMBER_OF_ATTEMPTS
+     * @throws ExecutionException
+     */
+    public boolean hasExceededMaxAttempts(String username) throws ExecutionException {
+        return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS;
+    }
 
 }
+
+
+
+/*
+---    CACHE ----
+USER(key)       ATTEMPTS(value)
+user1            1+1+1
+user2             1
+user3             2
+ */
