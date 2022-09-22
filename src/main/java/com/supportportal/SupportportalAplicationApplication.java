73b175d088c3f830abb6f14bf2a803c26aef182e
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-09-22 23:46:10 +0300

    generate folders according to path in USER_FOLDER constant

--- src/main/java/com/supportportal/SupportportalAplicationApplication.java
+++ src/main/java/com/supportportal/SupportportalAplicationApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.File;
+
+import static com.supportportal.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 public class SupportportalAplicationApplication {
 
@@ -12,6 +16,9 @@ public class SupportportalAplicationApplication {
 
 		SpringApplication.run(SupportportalAplicationApplication.class, args);
 		System.out.println("starting");
+		//any time the app is run it will generate this folders for me
+		//the folders mentioned in the path that is in the constant USER_FOLDER
+		new File(USER_FOLDER).mkdirs();
 	}
 
 	//35video 15:01
