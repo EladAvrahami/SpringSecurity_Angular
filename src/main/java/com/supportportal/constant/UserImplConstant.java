fece0f53b8767ab2796d25a603946fc38122733d
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-10-09 01:46:08 +0300

    profile image url to robohash api fix

--- src/main/java/com/supportportal/constant/UserImplConstant.java
+++ src/main/java/com/supportportal/constant/UserImplConstant.java
@@ -1,7 +1,7 @@
 package com.supportportal.constant;
 
 public class UserImplConstant {
-    public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile";
+    public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile/";
     public static final String USERNAME_ALREADY_EXISTS = "Username already exists";
     public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
     public static final String NO_USER_FOUND_BY_USERNAME = "No user found by username: ";
